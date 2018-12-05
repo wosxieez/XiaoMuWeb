b5ff947abb937d4cb62e8831d637d19b6fdf1bd6
coco <wosxieez@qq.com>
2018-12-05 16:41:22 +0800

    增加注册界面

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
+Vue.use(iView)
 Vue.config.productionTip = false
-
 
 new Vue({
   router, 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>注册界面</div>
+  <div class="content">
+    <Card title="用户注册" style="width: 400px; height: 300px">
+      <Form>
+        <FormItem>
+          <i-input>hello</i-input>
+        </FormItem>
+        <FormItem>
+          <i-input>hello</i-input>
+        </FormItem>
+        <FormItem>
+          <i-input>hello</i-input>
+        </FormItem>
+        <i-button>确定</i-button>
+      </Form>
+    </Card>
+  </div>
 </template>
-
 <script>
 export default {
   name: 'Register'
 }
 </script>
 
-<style lang="less" scoped>
-
+<style>
+.content {
+  background: #ffdd00;
+  height: 768px;
+  text-align: center
+}
 </style>
